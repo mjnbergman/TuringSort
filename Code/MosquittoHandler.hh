b4edbe2fc2e00bfb7b3e3aec1f305e08c732b3c1
Rens Klaren <rensk@20172821.campus.tue.nl>
2019-03-29 13:44:42 +0100

    

--- Code/MosquittoHandler.hh
+++ Code/MosquittoHandler.hh
@@ -5,7 +5,7 @@
 
 class MosquittoHandler {
 public:
-	MosquittoHandler();
+	MosquittoHandler(const char *host, int port, int keepAlive);
 	void takeItem();
 	void startSequence();
 	void reboot();
@@ -17,6 +17,6 @@ private:
 	int port;
 	int keepAlive;
 	struct mosquitto mosq*;
-	void connectCallback();
-	void messageCallback();
+	void connectCallback(struct mosquitto *mosq, void *userdata, int result);
+	void messageCallback(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *message);
 }
