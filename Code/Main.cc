73d3f9ef676d50a07377b4410aeb0d13cb4b7f42
20172525 <38973385+20172525@users.noreply.github.com>
2019-03-29 11:10:45 +0100

    Update Main.cc
    added motor control to main

--- Code/Main.cc
+++ Code/Main.cc
@@ -57,6 +57,7 @@ int main(){
 
 
 	  int dataPin = 5, latchPin = 6, clockPin = 7;
+    byte motorBitsToSend = 0; //bits to send to the shift register for motor control
 
 
 	  System s(locator.set(runtime).set(illegal_handler));
@@ -71,17 +72,56 @@ int main(){
 		//	  TimerHelper t(s.pusherSystem.p1.timer.out.timeout);
 	  };
 
+    s.belt.control.motor.in.turnClockwise = [] {
+        turnMotor(0, false);
+    };
+
+    s.belt.control.motor.in.turnCounterClockwise = [] {
+        turnMotor(0, true);
+    };
+
+    s.belt.control.motor.in.off = [] {
+        stopMotor(0);
+    };
+
 	  s.pusherSystem.p1.motor.in.turnClockwise = [] {
 			  // Draai motor 1
+        turnMotor(1, false);
 	  };
 
 	  s.pusherSystem.p2.motor.in.turnClockwise = [] {
 			  // Draai motor 2
+        turnMotor(2, false);
 	  };
 
 	  s.pusherSystem.p3.motor.in.turnClockwise = [] {
 			  // Draai motor 3
+        turnMotor(3, false);
 	  };
+
+    s.pusherSystem.p1.motor.in.turnCounterClockwise = [] {
+        turnMotor(1, true);
+    };
+
+    s.pusherSystem.p2.motor.in.turnCounterClockwise = [] {
+        turnMotor(2, true);
+    };
+
+    s.pusherSystem.p3.motor.in.turnCounterClockwise = [] {
+        turnMotor(3, true);
+    };
+
+    s.pusherSystem.p1.motor.in.off = [] {
+        stopMotor(1);
+    };
+
+    s.pusherSystem.p2.motor.in.off = [] {
+        stopMotor(2);
+    };
+
+    s.pusherSystem.p3.motor.in.off = [] {
+        stopMotor(3);
+    };
 
 	  s.pusherSystem.p1.timer.in.createTimer = [] (double time) {
 			//  TimerHelper t(s.pusherSystem.p1.timer.out.timeout);
@@ -106,3 +146,35 @@ int main(){
 	return 0;
 }
 
+//writes the motorBitsToSend to the shift register for motor control
+void registerWrite() {
+  //turn off the output so the pins don't light up while the bits are being shifted
+  digitalWrite(latchPin, LOW);
+
+  //shifts the previous bits out and writes bitsToSend (most significant bit first)
+  shiftOut(dataPin, clockPin, MSBFIRST, motorBitsToSend);
+
+  //turn on the output again
+  digitalWrite(latchPin, HIGH);
+}
+
+//turns a motor
+//requires the motor number 0-3
+//requires a boolean for turning direction turnLeft = false for clockwise
+void turnMotor(int motor, bool turnLeft) {
+  int bitToWrite = 0;
+  bitToWrite = motor * 2;
+  bitWrite(motorBitsToSend, bitToWrite, turnLeft);
+  bitWrite(motorBitsToSend, bitToWrite + 1, !turnLeft);
+  registerWrite();
+}
+
+//stops a motor
+//requires the motor number 0-3
+void stopMotor(int motor) {
+  int bitToWrite = 0;
+  bitToWrite = motor * 2;
+  bitWrite(bitsToSend, bitToWrite, false);
+  bitWrite(bitsToSend, bitToWrite + 1, false);
+  registerWrite();
+}
